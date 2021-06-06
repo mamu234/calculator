fc459b88ebbc6796fcb2e283990c555f52087cf2
caroline <pcmaunda@gmail.com>
2021-06-06 22:08:29 +0300

    added comments

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-
+//popping up prompt boxes for input and then an alert with the calculated result
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -10,6 +10,10 @@ var result = add(number1, number2);
 alert(result);
 
 
+//write a function that calculates body mass index (BMI) using two arguments. 
+//Then write another function that prompts the user for their height and weight,
+// uses the BMI function and alerts the user of their BMI
+
 var divide = function(height , weight){ 
     return height /weight;
 };
@@ -18,6 +22,8 @@ var weight = parseInt(prompt("Enter your weight"))
 var calculate = divide(height,weight)
 alert(calculate);
 
+
+//Write a function that converts temperature between Celsius and Fahrenheit.
 var temperature = function(celsius ,fahrenheit){
     return celsius / fahrenheit;
 }
@@ -25,3 +31,5 @@ var temperature = function(celsius ,fahrenheit){
  var fahrenheit = parseInt(prompt("Enter fahrenheit"));
  var outcome = temperature(celsius,fahrenheit);
  alert(outcome +32);
+
+ //Create a simple calculator that calculates if a number is even or not. 
