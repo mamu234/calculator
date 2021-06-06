4869d49d36a6f5e1c3b07e1d61be1c789b0aa15f
caroline <pcmaunda@gmail.com>
2021-06-06 21:41:15 +0300

    new prompt

--- main.js
+++ main.js
@@ -9,3 +9,19 @@ var number2 = parseInt(prompt("Enter another number:"));
 var result = add(number1, number2);
 alert(result);
 
+
+var divide = function(height , weight){ 
+    return height /weight;
+};
+var height = parseInt(prompt("Enter your height"));
+var weight = parseInt(prompt("Enter your weight"))
+var calculate = divide(height,weight)
+alert(calculate);
+
+var temperature = function(celsius ,fahrenheit){
+    return celsius / fahrenheit;
+}
+ var celsius = parseInt(prompt("Enter celsius"));
+ var fahrenheit = parseInt(prompt("Enter fahrenheit"));
+ var outcome = temperature(celsius,fahrenheit);
+ alert(outcome +32);
